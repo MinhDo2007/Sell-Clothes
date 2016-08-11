d4113b8e90102e1454b397ca5a58e8e09feda877
MinhDo2007 <minhdt.cntt@gmail.com>
2016-08-11 17:49:11 +0700

    heroku

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module SellClothes
     config.action_mailer.perform_deliveries = true
     config.action_mailer.delivery_method = :smtp
 
-    config.action_mailer.default_url_options = {host: "sitepoint-devise.herokuapp.com"}
+    config.action_mailer.default_url_options = {host: "secret-temple-34805.herokuapp.com"}
     config.action_mailer.smtp_settings = {
       :address => 'smtp.sendgrid.net',
       :port => "587",
