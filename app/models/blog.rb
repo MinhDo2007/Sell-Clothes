915cb5e532a276679e57268e5a35df081ee11a3a
MinhDo2007 <minhdt.cntt@gmail.com>
2016-10-27 16:51:35 +0700

    page-pilling

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,14 @@
 class Blog < ActiveRecord::Base
   belongs_to :user
   mount_uploader :image, ImageUploader
+
+  scope :order_blogs, ->{order(created_at: :desc)}
+
+  class << self
+    def get_four_last_blogs blog_arr
+      blog_arr = blog_arr.sort{|x,y| x <=> y}
+      blog_arr.map.with_index {|x, i| x if i < 3}
+      blog_arr = blog_arr.compact
+    end
+  end
 end
